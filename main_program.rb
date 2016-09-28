d6f8d6ad28ebec696224beab0537df4ea55a4cf7
Kyle Boling <kmboling@gmail.com>
2016-09-28 19:21:48 -0400

    Added menu method

--- main_program.rb
+++ main_program.rb
@@ -1,4 +1,6 @@
 system "clear"
+
+def display_menu
 puts "*************************************************"
 puts "****  Welcome to the Car Rental Application  ****"
 puts "*************************************************"
@@ -9,11 +11,13 @@ puts "**** 3. Option 3"
 puts "**** 4. Option 4"
 
 option = gets.chomp.to_i
-
 	case option
-		when 1 then puts "1"
-		when 2 then puts "2"
-		when 3 then puts "3"
-		when 4 then puts "4"
-		else puts "Invalid selection, please choose from menu"
+			when 1 then puts "1"
+			when 2 then puts "2"
+			when 3 then puts "3"
+			when 4 then puts "4"
+			else puts "Invalid selection, please choose from menu"
 	end
+end
+display_menu
+
