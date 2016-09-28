6a21f8a01fbd1df5f9c6ae15a4737a1326d20b5f
Kyle Boling <kmboling@gmail.com>
2016-09-28 18:51:09 -0400

    Implemented generic menu

--- main_program.rb
+++ main_program.rb
@@ -1,1 +1,13 @@
-puts "hello"
+system "clear"
+puts "*************************************************"
+puts "****  Welcome to the Car Rental Application  ****"
+puts "*************************************************"
+puts "****  Please select from the following menu: ****"
+puts "**** 1. Option 1"
+puts "**** 2. Option 2"
+puts "**** 3. Option 3"
+puts "**** 4. Option 4"
+
+option = gets.chomp
+
+puts "Your chosen option #{option}"
